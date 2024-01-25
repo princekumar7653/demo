13087d5dcafdbdefabc54865216ebc78d07bc1e7
princekumar7653 <156938370+princekumar7653@users.noreply.github.com>
2024-01-25 19:58:11 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+//alert(7+5) // we are using nodejs, not browser
+
+console.log(3 +7); // code redability should be high
+
+
+console.log("prince")
+
+let name = "prince"
+let age = 18
+
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => tru/false
+// null => standalone value
+// undefined => 
+// symbol => uniqe
+
+// object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null); // object
