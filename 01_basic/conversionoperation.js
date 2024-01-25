6053a16c2ec80b10d587b187632820500a8b080d
princekumar7653 <156938370+princekumar7653@users.noreply.github.com>
2024-01-25 20:58:26 +0000

    conversion of operation

---
+++ 01_basic/conversionoperation.js
@@ -0,0 +1,36 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33avc" => nan
+// true => 1; false => 0
+
+ let isLoggedIn = "prince"
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// ""=> false
+// "prince" => true
+
+let someNumber = 34
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
