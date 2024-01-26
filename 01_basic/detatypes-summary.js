eba7a0824e8d8dbeef294ab5a5b22a2818d42f99
princekumar7653 <156938370+princekumar7653@users.noreply.github.com>
2024-01-26 13:18:08 +0000

    detatypes

---
+++ 01_basic/detatypes-summary.js
@@ -0,0 +1,41 @@
+// primitive detatypes
+
+// 7 types: string, number, boolean, null, undefined, BigInt, symbol
+
+const score = 100
+
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null 
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+//const bigNumber = 5667727763555363888376n // bigInt
+
+//console.log(bigNumber);
+
+
+
+
+//reference (Non primitive)
+
+// aaray, objects, functions
+
+const heros = ["ajay", "vijay", "manoj"]
+
+let myObj = {
+    fullName: "prince singh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("prince singh");
+}
+
+console.log(typeof scoreValue);
+
