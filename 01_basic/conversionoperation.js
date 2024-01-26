72d2682ab818153792a78597ffe65517c608cc89
princekumar7653 <156938370+princekumar7653@users.noreply.github.com>
2024-01-26 09:47:58 +0000

    operation

--- 01_basic/conversionoperation.js
+++ 01_basic/conversionoperation.js
@@ -25,8 +25,51 @@ let valueInNumber = Number (score)
 let someNumber = 34
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ******************************* operation ******************************
+
+let value = 3
+
+let negValue = -value
+console.log(negValue);
+
+
+//console.log(2+3);
+//console.log(2-2);
+//console.log(2*4);
+//console.log(5**5);
+//console.log(5/50);
+//console.log(2%6);
+
+let str1 = "prince"
+
+let str2 = " singh bhumihar"
+
+let str3 = str1 + str2 
+
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("2" + 2);
+// console.log("2" + 2 + 3);
+// console.log(2 + 2 + "3");
+
+//console.log((3+4) * 5 % 3);
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+
+++gameCounter;
+
+console.log(gameCounter);
 
 
 
