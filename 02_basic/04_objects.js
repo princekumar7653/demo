52108c8f702fe181f1db7b68d0c9e676c96628f3
princekumar7653 <156938370+princekumar7653@users.noreply.github.com>
2024-02-01 18:35:35 +0000

    objects2

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,71 @@
+// single tn
+
+//const finderUsr = new Object()
+
+const finderUser = {}
+
+finderUser.id = "234abc"
+finderUser.name = "prince"
+finderUser.isLoggedIn = false
+//console.log(finderUser);
+
+const allUsers = {
+    email: "prince123@gmail.com",
+    fullName: {
+        userfullname: {
+            firstname: "prince",
+            lastname: "singh"
+        }
+
+    }
+}
+
+
+//console.log(allUsers.fullName.userfullname.firstname);
+
+
+const obj1 = {1: "a", 2: "b", 3: "c"}
+const obj2 = {4: "d", 5: "e", 6: "f"}
+const obj4 = {7: "d", 8: "e", 9: "f"}
+
+//const obj3 = {obj1, obj2}
+
+//const obj3 = Object.assign(obj1, obj2)
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+
+
+
+//console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "prince@gmail.com"
+    },
+    {
+        id: 1,
+        email: "prince@gmail.com"
+    },
+    {
+        id: 1,
+        email: "prince@gmail.com"
+    },
+]
+
+//users[1].email
+
+console.log(users[1].id)
+
+// console.log(finderUser);
+
+// console.log(Object.keys(finderUser));
+// console.log(Object.values(finderUser));
+// console.log(Object.entries(finderUser));
+
+console.log(finderUser.hasOwnProperty('isLogged'));
+
+
+
+
