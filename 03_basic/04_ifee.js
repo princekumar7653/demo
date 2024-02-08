f914041976c58ac7e01e8a9a10507338940b2f8d
princekumar7653 <156938370+princekumar7653@users.noreply.github.com>
2024-02-08 19:23:07 +0000

    ifee

---
+++ 03_basic/04_ifee.js
@@ -0,0 +1,15 @@
+
+
+
+(function chai(){
+    console.log("db connected");
+})();
+
+
+( (name) => {
+    console.log(`Db not connected ${name}`);
+ })('prince')
+
+ const arr = [1, 2, 4, 6, 7]
+
+ console.log([arr]);
