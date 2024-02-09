61da5ebef08c4ae32de7890a9ea83c265909f31f
princekumar7653 <156938370+princekumar7653@users.noreply.github.com>
2024-02-09 18:33:27 +0000

    one

---
+++ 04_controlflow/one.js
@@ -0,0 +1,38 @@
+const tempareture = 41
+
+if (tempareture === 41){
+    console.log("less than 50");
+}else{
+    console.log("temperature greater than 50");
+}
+
+//console.log("execute");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+const score = 200
+
+if(score > 100){
+    const power = "fly"
+    //console.log(`user power: ${power}`);
+}
+
+//console.log(`user power: ${power}`);
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log("test2")
+
+if(balance < 500){
+    console.log("less than 500");
+
+}else if(balance < 750){
+    console.log("less than 750");
+}else if(balance < 950){
+    console.log("less than 950");
+}else{
+    console.log("less than 1200");
+}
+
+
+
