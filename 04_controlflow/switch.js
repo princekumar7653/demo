10318e5a3d2db3aa40bf809b9f903542adfffe22
princekumar7653 <156938370+princekumar7653@users.noreply.github.com>
2024-02-10 09:02:06 +0000

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,59 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 4
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+        
+//         break;
+//     case 2:
+//         console.log("feb");
+        
+//         break;
+//     case 3:
+//         console.log("march");
+        
+//         break;
+//     case 4:
+//         console.log("april");
+        
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+
+const month = "march"
+
+switch (month) {
+    case "1jan":
+        console.log("january");
+        
+        break;
+    case "2feb":
+        console.log("feb");
+        
+        break;
+    case "march":
+        console.log("march");
+        
+        break;
+    case "4april":
+        console.log("april");
+        
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
