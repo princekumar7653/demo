af8a9c63c548632a6e1feaa645a2473a76338695
princekumar7653 <156938370+princekumar7653@users.noreply.github.com>
2024-02-12 17:41:52 +0000

    for of

---
+++ 05_loops/three.js
@@ -0,0 +1,39 @@
+const arr = [1, 2, 3, 4, 5, 6]
+
+for (const array of arr) {
+    //console.log(`array is ${array}`);
+}
+
+
+const greetings = "prince"
+
+for (const greet of greetings) {
+    //console.log(`my name is: ${greet}`);
+}
+
+const map = new Map()
+
+map.set('in', "india")
+map.set('USA', "United states of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+//console.log(map);
+
+for (const [key, value] of map) {
+    //console.log(key, ':-', value);
+}
+
+const myObject = {
+    'game': 'NFS',
+    'game2': 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+// }
+
+
+
+
