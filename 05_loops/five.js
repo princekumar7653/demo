690016ad91afd50337e4011e671eb3e6437048d6
princekumar7653 <156938370+princekumar7653@users.noreply.github.com>
2024-02-20 08:05:47 +0000

    five

---
+++ 05_loops/five.js
@@ -0,0 +1,42 @@
+const arr = ['js', "java", "cpp", "python"]
+
+// arr.forEach( function (item){
+//     console.log(item);
+// } );
+
+// arr.forEach( (item) => {
+//     console.log(item);
+// } );
+
+// function myPrint(val){
+//     console.log(val);
+// }
+
+//arr.forEach (myPrint)
+
+// arr.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// } );
+
+const myObject = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myObject.forEach( (item) => {
+    console.log(item.languageFileName);
+} );
+
+
