82222377a32f590b28725102ebe99d6bc968741f
Prince Singh <chanchalgopalkrishna42@gmail.com>
2024-04-05 18:42:56 +0530

    dd

--- 01_basic/04_comparison.js
+++ 01_basic/04_comparison.js
@@ -1,22 +1,22 @@
-// console.log(2 > 3); //false
-// console.log(4 >= 3); //true
-// console.log(2 < 1); // false
-// console.log(2 == 1); //false
-// console.log(2 != 1); //true
+console.log(2 > 3); //false
+console.log(4 >= 3); //true
+console.log(2 < 1); // false
+console.log(2 == 1); //false
+console.log(2 != 1); //true
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log("2" > 1);
+console.log("02" > 1);
 
 
 // is trah ke conversion kbhi v confusion me dal sakta hai
 
-// console.log(null > 0);
-// console.log(null == 0);
-// console.log(null >= 0);
+    console.log(null > 0);
+    console.log(null == 0);
+    console.log(null >= 0);
 
-// console.log(undefined > 0);
-// console.log(undefined == 0);
-// console.log(undefined < 0);
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
 
 
 // === uske detatype ko v check karta hai
